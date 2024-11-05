488d8de33126222611763a5a20023175ecb81a8a
Elena Zhuvak <elenkazh@gmail.com>
2024-11-05 19:03:49 +0000

    create components folder

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import 'modern-normalize';
+import Description from './Description/Description';
+import Options from './Options/Options';
+import Feedback from './Feedback/Feedback';
+
+const App = () => {
+  return (
+    <div>
+      <Description />
+      <Options />
+      <Feedback />
+    </div>
+  )
+}
+
+export default App
